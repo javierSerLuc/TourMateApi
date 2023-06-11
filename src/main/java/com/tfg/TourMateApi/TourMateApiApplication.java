37192364dd7adf862014410a6329f8387c601820
Javier Serrano <javier.serluc@gmail.com>
2023-06-11 18:29:58 +0200

    Initial commit

---
+++ src/main/java/com/tfg/TourMateApi/TourMateApiApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.TourMateApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourMateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourMateApiApplication.class, args);
+	}
+
+}
