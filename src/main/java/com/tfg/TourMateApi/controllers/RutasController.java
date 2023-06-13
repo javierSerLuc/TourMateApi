f83561683f0b69618633240a92818b92b48005b7
Javier Serrano <javier.serluc@gmail.com>
2023-06-13 20:19:31 +0200

    Coger json desde controller funcionando

--- src/main/java/com/tfg/TourMateApi/controllers/RutasController.java
+++ src/main/java/com/tfg/TourMateApi/controllers/RutasController.java
@@ -1,24 +1,43 @@
 package com.tfg.TourMateApi.controllers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tfg.TourMateApi.models.Poi;
 import com.tfg.TourMateApi.models.Ruta;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class RutasController {
 
     @GetMapping("/api/getRutas")
     public Ruta genRutas(){
-        Poi demo1 = new Poi("Nido del Búho","bar","-36.095.761","371.899.118",	50.0,	"['Mo-Sa 10:00-14:00']");
-        Poi demo2 = new Poi("Buzón de Correos", "post_box", "-36.128.731", "371.815.584", 23.0, "['24/7']");
-        //return ResponseEntity.ok("End Point encontrado");
-        Ruta rutaDemo = new Ruta();
-        rutaDemo.addPoi(demo1);
-        rutaDemo.addPoi(demo2);
+        Ruta rutaDemo = new Ruta(cargarPoisJson());
         return rutaDemo;
-        //return demo;
     }
+
+
+    public List<Poi> cargarPoisJson(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            File file = new File("src/main/resources/poisJson/response-100.json");
+            JsonNode jsonNode = objectMapper.readTree(file);
+            JsonNode poisProperty = jsonNode.get("pois");
+            Poi[] poisArray = objectMapper.convertValue(poisProperty, Poi[].class);
+            return Arrays.asList(poisArray);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
 
 }
